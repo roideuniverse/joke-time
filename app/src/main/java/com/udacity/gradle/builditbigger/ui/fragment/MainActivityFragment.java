47dca59669c0ba18ad76020446543bea76d468e5
Kaushik Saurabh <roideuniverse@gmail.com>
2016-01-28 21:22:00 -0500

    style:fix style

--- app/src/main/java/com/udacity/gradle/builditbigger/ui/fragment/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/ui/fragment/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package com.udacity.gradle.builditbigger.ui.fragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +14,17 @@ import com.udacity.gradle.builditbigger.R;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment
+{
 
-    public MainActivityFragment() {
+    public MainActivityFragment()
+    {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
